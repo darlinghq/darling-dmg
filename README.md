672b62270eb19abb43a06178dc1deccf4eceff5b
Lubos Dolezel <lubos@dolezel.info>
2013-06-16 20:51:04 +0200

    Updated README & missing file

---
+++ README.md
@@ -0,0 +1,18 @@
+# darling-dmg
+
+This project allows ordinary users to directly mount OS X disk images under Linux via FUSE.
+
+## Supported file types
+
+* DMG (UDIF) files containing an Apple Disk Image.
+* Apple Disk Images containing an HFS+/HFSX file system.
+* HFS+/HFSX file systems (incl. file systems embedded within HFS).
+
+As you can see, the list is recursive. That means darling-dmg, can mount DMG files or unpacked DMG files or a single partition carved out of the latter.
+
+Read only access only.
+
+## Usage
+
+    darling-dmg <file-to-mount> <where-to-mount> [FUSE arguments]
+
---
+++ src/main-fuse.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_FUSE_H
+#define MAIN_FUSE_H
+#define FUSE_USE_VERSION 26
+
+#include <fuse.h>
+
+static void showHelp(const char* argv0);
+static void openDisk(const char* path);
+
+int hfs_getattr(const char* path, struct stat* stat);
+int hfs_readlink(const char* path, char* buf, size_t size);
+int hfs_open(const char* path, struct fuse_file_info* info);
+int hfs_read(const char* path, char* buf, size_t bytes, off_t offset, struct fuse_file_info* info);
+int hfs_release(const char* path, struct fuse_file_info* info);
+int hfs_opendir(const char* path, struct fuse_file_info* info);
+int hfs_readdir(const char* path, void* buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info* info);
+int hfs_releasedir(const char* path, struct fuse_file_info* info);
+
+#endif
+
