5b8259b93277e4bcc6782e74b1b4c9e3ddd45ca6
Lubos Dolezel <lubos@dolezel.info>
2020-02-24 13:52:18 +0100

    Support most common hdiutil command line options

--- src/main-hdiutil.cpp
+++ src/main-hdiutil.cpp
@@ -7,6 +7,7 @@
 #include <elfcalls.h>
 #include <sys/wait.h>
 #include <spawn.h>
+#include <getopt.h>
 
 static void printMount();
 
@@ -41,31 +42,82 @@ static void printHelp()
 {
 	std::cerr << "Usage: hdiutil <action>\n\n";
 	std::cerr << "Possible actions:\n"
-		"\tattach <file>\n"
+		"\tattach [options] <file>\n"
 		"\t\tMounts a .dmg file <file> and prints the mount locaton\n"
-		"\tdetach <mount-path>\n"
+		"\tdetach [options] <mount-path>\n"
 		"\t\tUnmounts a .dmg file mounted at <mount-path>\n";
 
 	exit(1);
 }
 
 static std::string g_mountDir;
+static bool puppetstrings = false;
 
 static int doAttach(int argc, char** argv)
 {
-	if (argc != 3)
+	if (argc < 3)
 		printHelp();
 
+	static const struct option longopts[] = {
+		{ "puppetstrings", no_argument, NULL, 1 },
+		{ "mountpoint", required_argument, NULL, 2 },
+		{ "mountroot", required_argument, NULL, 3 },
+		{ "quiet", no_argument, NULL, 4 },
+		{ "nobrowse", no_argument, NULL, 5 },
+		{ "noautoopen", no_argument, NULL, 5 },
+		{ NULL, 0, NULL, 0 }
+	};
+
 	std::string prefix, mount, dmg;
-	std::string mountname;
+	std::string mountname, mountroot;
 	const char *p, *p2;
 	int fd;
 	char output[] = "/tmp/hdiutilXXXXXX";
-	const char* args[5];
+	const char* args[7];
+	bool quiet = false;
 
-	mount = "/Volumes/";
+	mountroot = "/Volumes";
 
-	dmg = argv[2];
+	// Skip "attach"
+	argc--;
+	argv++;
+
+	int ch;
+	while ((ch = getopt_long_only(argc, argv, "", longopts, NULL)) != -1)
+	{
+		switch (ch)
+		{
+			case 1:
+				puppetstrings = true;
+				break;
+			case 2:
+				mount = optarg;
+				break;
+			case 3:
+				mountroot = optarg;
+				break;
+			case 4:
+				quiet = true;
+				break;
+			case 5:
+				// We don't implement opening Finder yet, so this is our default
+				break;
+			case 0:
+				break;
+			default:
+				std::cerr << "Got ch " << ch << std::endl;
+				printHelp();
+		}
+	}
+
+	if (optind != argc-1)
+	{
+		std::cerr << "Optind is " << optind << std::endl;
+		std::cerr << "Arg at optind is " << argv[optind] << std::endl;
+		printHelp();
+	}
+
+	dmg = argv[optind];
 
 	if (access(dmg.c_str(), R_OK) != 0)
 	{
@@ -73,26 +125,32 @@ static int doAttach(int argc, char** argv)
 		return 1;
 	}
 
-	p = strrchr(argv[2], '/');
+	p = strrchr(argv[optind], '/');
 	if (p == nullptr)
 		p = argv[2];
 	else
 		p++;
 
-	p2 = strrchr(argv[2], '.');
+	p2 = strrchr(argv[optind], '.');
 	if (p2 == nullptr)
-		p2 = argv[2] + strlen(argv[2]);
+		p2 = argv[optind] + strlen(argv[optind]);
 
 	mountname = std::string(p, p2-p);
-	mount += mountname;
-	g_mountDir = "/Volumes/";
-	g_mountDir += mountname;
+
+	if (mount.empty())
+	{
+		mount = mountroot;
+		if (mount[mount.length()-1] != '/')
+			mount += '/';
+		mount += mountname;
+	}
 
 	if (mkdir(mount.c_str(), 0777) == -1 && errno != EEXIST)
 	{
 		std::cerr << "Cannot mkdir " << mount << std::endl;
 		return 1;
 	}
+	g_mountDir = mount;
 
 	fd = mkstemp(output);
 
@@ -109,8 +167,12 @@ static int doAttach(int argc, char** argv)
 	__darling_vchroot_expand(mount.c_str(), linux_path);
 	args[2] = linux_path;
 
+	std::cerr << "Will pass " << args[2] << std::endl;
+
 	args[3] = "-f"; // Fork has to be done by Darling code, otherwise the LKM will not talk to us any more
-	args[4] = nullptr;
+	args[4] = "-o";
+	args[5] = "nonempty";
+	args[6] = nullptr;
 
 	addFusermountIntoPath();
 
@@ -143,7 +205,17 @@ static int doAttach(int argc, char** argv)
 
 static void printMount()
 {
-	std::cout << g_mountDir << std::endl;
+	if (!puppetstrings)
+		std::cout << g_mountDir << std::endl;
+	else
+	{
+		// FIXME: This is kind of fake...
+		// puppetstrings should produce output like:
+		// /dev/disk1              GUID_partition_scheme           
+		// /dev/disk1s1            Apple_HFS                       /Volumes/MachOView
+		std::cout << "/dev/disk1\tGUID_partition_scheme\t\n";
+		std::cout << "/dev/disk1s1\tApple_HFS\t" << g_mountDir << std::endl;
+	}
 }
 
 extern "C"
@@ -157,12 +229,38 @@ static int doDetach(int argc, char** argv)
 {
 	pid_t pid;
 	int (*elf_posix_spawnp)(pid_t* pid, const char* path, const posix_spawn_file_actions_t *file_actions, const posix_spawnattr_t *attrp, char *const argv[], char *const envp[]);
-	const char* pargv[] = { "fusermount", "-u", argv[2], nullptr };
 
-	if (argc != 3)
+	if (argc < 3)
 		printHelp();
 
+	static const struct option longopts[] = {
+		{ "quiet", no_argument, NULL, 1 },
+		{ NULL, 0, NULL, 0 }
+	};
+
+	// Skip "detach"
+	argc--;
+	argv++;
+
+	int ch;
+	while ((ch = getopt_long_only(argc, argv, "", longopts, NULL)) != -1)
+	{
+		switch (ch)
+		{
+			case 1:
+				break;
+			case 0:
+				break;
+			default:
+				printHelp();
+		}
+	}
+
 	addFusermountIntoPath();
+
+	char linux_path[4096];
+	__darling_vchroot_expand(argv[optind], linux_path);
+	const char* pargv[] = { "fusermount", "-u", linux_path, nullptr };
 
 	*((void**)(&elf_posix_spawnp)) = _elfcalls->dlsym_fatal(nullptr, "posix_spawnp");
 	if (elf_posix_spawnp(&pid, "fusermount", nullptr, nullptr, (char* const*) pargv, environ) != 0)
@@ -174,7 +272,7 @@ static int doDetach(int argc, char** argv)
 	{
 		int status;
 		waitpid(pid, &status, 0);
-		rmdir(argv[2]);
+		rmdir(argv[optind]);
 
 		return 0;
 	}
